7f28debf09c48674aaa1870a9da3103588fce214
seokhochoi <seokho9473@gmail.com>
2024-09-09 20:48:57 +0900

    feat: 하단 Nav 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { Suspense } from 'react';
-import styles from './App.module.scss';
 import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className={styles.renderTest}>
-      <Suspense fallback={<div>Loading..</div>}>
-        <Outlet />
-      </Suspense>
-    </div>
+    <Suspense fallback={<div>Loading..</div>}>
+      <Outlet />
+    </Suspense>
   );
 }
 
