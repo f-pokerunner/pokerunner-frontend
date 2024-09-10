7da6a7d002288dbe607561dab07470e26164b358
heesu <hspyo@zimssa.com>
2024-09-10 21:17:55 +0900

    feat: Intro 화면에서 하단바 노출되지 않도록 수정

--- src/layouts/default/index.js
+++ src/layouts/default/index.js
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 import styles from './index.module.scss';
@@ -7,12 +7,16 @@ import Nav from '../../components/nav';
 const cx = classNames.bind(styles);
 
 export default function Default() {
+  const location = useLocation();
+
+  const isIntroPage = location.pathname === '/intro';
+
   return (
     <div className={cx('commonLayout')}>
       <main>
         <Outlet />
       </main>
-      <Nav />
+      {!isIntroPage && <Nav />}
     </div>
   );
 }
