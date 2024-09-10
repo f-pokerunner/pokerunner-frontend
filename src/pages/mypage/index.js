1bd6d6b7d382c9a0468da7e7f79a77d9253146f7
seokhochoi <seokho9473@gmail.com>
2024-09-10 18:42:56 +0900

    feat: InfoCard 컴포넌트 구현

--- src/pages/mypage/index.js
+++ src/pages/mypage/index.js
@@ -1,7 +1,10 @@
+import InfoCard from '../../components/InfoCard';
+
 export default function Mypage() {
   return (
     <>
       <h1>Mypage Component</h1>
+      <InfoCard backgroundColor="#446934" />
     </>
   );
 }
