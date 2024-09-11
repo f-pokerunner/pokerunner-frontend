88a852518291209fd7438e25c603b7a3031daf2e
seokhochoi <seokho9473@gmail.com>
2024-09-11 12:31:23 +0900

    feat: InfoCard UI 틀어짐 수정

--- src/pages/mypage/index.js
+++ src/pages/mypage/index.js
@@ -1,10 +1,13 @@
+import classNames from 'classnames/bind';
 import InfoCard from '../../components/InfoCard';
+import styles from './index.module.scss';
 
 export default function Mypage() {
+  const cx = classNames.bind(styles);
+
   return (
-    <>
-      <h1>Mypage Component</h1>
+    <div className={cx('area')}>
       <InfoCard backgroundColor="#446934" />
-    </>
+    </div>
   );
 }
