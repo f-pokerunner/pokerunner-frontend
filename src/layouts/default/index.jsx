2226677352979e2bc358fafbb2b7d608c1ec83dd
seokhochoi <seokho9473@gmail.com>
2024-09-15 00:23:17 +0900

    임시 테스트

--- src/layouts/default/index.jsx
+++ src/layouts/default/index.jsx
@@ -1,33 +1,3 @@
-// import { Outlet } from 'react-router-dom';
-// import classNames from 'classnames/bind';
-// import { useExperience } from '../../hooks/useExperience';
-// import styles from './index.module.scss';
-// import Nav from '../../components/nav';
-// import Popup from '../../components/popup';
-// import { ExperienceProvider } from '../../context/ExperienceContext';
-
-// const cx = classNames.bind(styles);
-
-// export default function Default() {
-//   const { showPopup, setShowPopup, message } = useExperience();
-
-//   return (
-//     <ExperienceProvider>
-//       <div className={cx('commonLayoutWrapper')}>
-//         {showPopup && (
-//           <Popup message={message} onClose={() => setShowPopup(false)} />
-//         )}
-
-//         <main className={cx('mainWrapper')}>
-//           <Outlet />
-//         </main>
-//         <div className={cx('navWrapper')}>
-//           <Nav />
-//         </div>
-//       </div>{' '}
-//     </ExperienceProvider>
-//   );
-// }
 import { Outlet } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import {
